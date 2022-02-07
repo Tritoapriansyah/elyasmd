22839be800fd0ce5c0f8f1bb1ef260e36c807d69
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2022-02-07 16:36:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -42,14 +42,14 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
-global.author = `elyas(bot)
+global.author = `Lolita(bot)
 
 
 
 
-ig: elyas_tzy`
+ig: trito_apriansyah`
 
-global.wm = 'by elyas'
+global.wm = 'by lolita'
 global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
 global.instagram = 'https://instagram.com/elyas_tzy'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
